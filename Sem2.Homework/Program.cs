d016fea9dac7fe7c9c0b517c846dee8d71051f75
earth962 <earth962@mail.ru>
2024-01-08 18:35:29 +0300

    Решение Задачи № 2

--- Sem2.Homework/Program.cs
+++ Sem2.Homework/Program.cs
@@ -1,14 +1,39 @@
-﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+﻿// // Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
 
-Console.Write("Введите число:" );
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число:" );
+// int number = Convert.ToInt32(Console.ReadLine());
 
-if (number %7 == 0 && number %23 ==0)
+// if (number %7 == 0 && number %23 ==0)
+// {
+//     Console.WriteLine($"число {number} кратно одновременно 7 и 23");
+// }
+// else
+// {
+//     Console.WriteLine($"число {number} не кратно одновременно 7 и 23");
+// }
+
+// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+Console.Write("Введите координату Х:" );
+int X = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату Y:" );
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if (X > 0 && Y > 0)
 {
-    Console.WriteLine($"число {number} кратно одновременно 7 и 23");
+    Console.WriteLine("Точка находится в I четверти");
 }
-else
+if (X < 0 && Y > 0)
 {
-    Console.WriteLine($"число {number} не кратно одновременно 7 и 23");
+    Console.WriteLine("Точка находится в II четверти");
 }
-
+if (X < 0 && Y < 0)
+{
+    Console.WriteLine("Точка находится в III четверти");
+}
+if (X > 0 && Y < 0)
+{
+    Console.WriteLine("Точка находится в IV четверти");
+}
